e8822b33b886eb6063a8cc8bb62fcf96c0c008ff
RAKESH x OFFICIAL <154747451+Rakeshxofficial@users.noreply.github.com>
2025-05-23 11:04:32 +0530

    Fix dependencies and Astro config

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,6 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import { SITE } from "./src/config";
 
-// https://astro.build/config
 export default defineConfig({
   site: SITE.website,
   integrations: [
@@ -16,7 +15,6 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
     shikiConfig: {
-      // For more themes, visit https://shiki.style/themes
       themes: { light: "min-light", dark: "night-owl" },
       wrap: true,
     },
@@ -27,14 +25,9 @@ export default defineConfig({
       exclude: ["@resvg/resvg-js"],
     },
   },
-  image: {
-    // Used for all Markdown images; not configurable per-image
-    // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
-    experimentalLayout: false,
-  },
   experimental: {
-    svg: true,
-    responsiveImages: true,
-    preserveScriptOrder: true,
+    viewTransitions: true,
+    assets: true,
+    svgs: true,
   },
 });
